64fc39d5507a1b6a084812cd43d532e53f53eb22
alebian <alebezdjian@gmail.com>
2016-03-30 20:05:53 -0300

    Added skill model

---
+++ models/src/main/java/ar/edu/itba/paw/models/Skill.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.models;
+
+public class Skill {
+	private String name;
+
+	public Skill(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
