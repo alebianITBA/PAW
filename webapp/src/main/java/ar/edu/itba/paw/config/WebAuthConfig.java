87cdf62818961399671ae4930b901d35e77ec1cc
alebian <alebezdjian@gmail.com>
2016-05-28 12:59:40 -0300

    Added safe token

--- webapp/src/main/java/ar/edu/itba/paw/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/config/WebAuthConfig.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.paw.config;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,13 +13,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import java.util.concurrent.TimeUnit;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 @ComponentScan("ar.edu.itba.paw.config")
 public class WebAuthConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 
   @Autowired
   private UserDetailsServiceImpl userDetailsService;
@@ -33,7 +40,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
         .loginPage("/")
       .and().rememberMe()
         .userDetailsService(userDetailsService)
-        .key("mysupersecretketthatnobodyknowsabout")
+        .tokenRepository(persistentTokenRepository())
         .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
       .and().logout()
         .logoutUrl("/logout")
@@ -42,6 +49,13 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
         .accessDeniedPage("/not_found")
       .and().csrf().disable();
   }
+  
+  @Bean
+  public PersistentTokenRepository persistentTokenRepository() {
+      JdbcTokenRepositoryImpl tokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+      tokenRepositoryImpl.setDataSource(dataSource);
+      return tokenRepositoryImpl;
+	}
 
   @Override
   public void configure(final WebSecurity web) throws Exception {
